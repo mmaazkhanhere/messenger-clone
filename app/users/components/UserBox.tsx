b0b562970a5d32c395c8064579fb4c6e1a08c56c
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-08-08 20:50:00 +0500

    user section created with avatar and name

---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,59 @@
+/* the UserBox component represents a UI box displaying user information. When clicked, 
+it sends a POST request to create a new conversation and then navigates to the conversation 
+page. */
+
+
+import Avatar from "@/app/components/Avatar";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+
+// Define the UserBoxProps interface to specify the props for UserBox component
+interface UserBoxProps {
+    data: User; // The user data to be displayed in the box
+}
+
+// Define the UserBox component using React.FC (functional component) pattern
+const UserBox: React.FC<UserBoxProps> = ({ data }) => {
+    // Get the router instance using the useRouter hook
+    const router = useRouter();
+    // State to manage loading state
+    const [isLoading, setIsLoading] = useState(false);
+
+    // Define a click event handler for the UserBox
+    const handleClick = useCallback(() => {
+        setIsLoading(true); // Set loading state to true
+        // Send a POST request to '/api/conversations' with user ID
+        axios.post('/api/conversations', {
+            userId: data.id
+        })
+            .then((data) => {
+                // After successful response, navigate to the conversation page
+                router.push(`/conversations/${data.data.id}`);
+            })
+            .finally(() => setIsLoading(false)); // Reset loading state
+    }, [data, router]);
+
+    // Return the UI representation of the UserBox
+    return (
+        <div
+            onClick={handleClick} // Attach click event handler to the box
+            className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg
+            transition cursor-pointer"
+        >
+            <Avatar user={data} /> {/* Render the Avatar component */}
+            <div className="min-w-0 flex-1">
+                <div className="focus:outline-none">
+                    <div className="flex justify-between items-center mb-1">
+                        <p className="text-sm font-medium text-gray-900">
+                            {data.name} {/* Display the user's name */}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserBox; // Export the UserBox component as default
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,15 @@
+import getUsers from '../actions/getUsers'
 import Sidebar from '../components/sidebar/Sidebar'
+import UserList from './components/UserList';
 
 export default async function UsersLayout({ children }: { children: React.ReactNode }) {
-    {/*Sidebar */ }
+
+    const users = await getUsers();
+
     return (
         <Sidebar>
             <div className="h-full" >
+                <UserList items={users} />
                 {children}
             </div>
         </Sidebar>
