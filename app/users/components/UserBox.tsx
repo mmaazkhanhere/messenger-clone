bfa2d97e463c354fc1656b877b307bdbd8871e82
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-08-16 17:09:21 +0500

    Loading modal created

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -4,6 +4,7 @@ page. */
 
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -34,22 +35,31 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
 
     // Return the UI representation of the UserBox
     return (
-        <div
-            onClick={handleClick} // Attach click event handler to the box
-            className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg
+        <>
+            {
+                isLoading && (
+                    <LoadingModal />
+                )
+            }
+
+            <div
+                onClick={handleClick} // Attach click event handler to the box
+                className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg
             transition cursor-pointer"
-        >
-            <Avatar user={data} /> {/* Render the Avatar component */}
-            <div className="min-w-0 flex-1">
-                <div className="focus:outline-none">
-                    <div className="flex justify-between items-center mb-1">
-                        <p className="text-sm font-medium text-gray-900">
-                            {data.name} {/* Display the user's name */}
-                        </p>
+            >
+                <Avatar user={data} /> {/* Render the Avatar component */}
+                <div className="min-w-0 flex-1">
+                    <div className="focus:outline-none">
+                        <div className="flex justify-between items-center mb-1">
+                            <p className="text-sm font-medium text-gray-900">
+                                {data.name} {/* Display the user's name */}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
+
     );
 }
 
